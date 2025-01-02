fa2ac470375d8b5fd2f7dc8738d14c108a4b4d0b
Y-hyundo <hyundo010488@naver.com>
2025-01-02 22:09:59 +0900

    Title: 재귀함수를 이용한 최댓값, Time: 3093ms, Memory: 10MB, Status: Time Limit Exceed - CodeTree

---
+++ 250102/재귀함수를 이용한 최댓값/maximum-value-with-recursive-function.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for(int i = 0; i < n; i++)
+            arr[i] = sc.nextInt();
+
+        System.out.println(find_max(arr, n - 1));
+    }
+
+    public static int find_max(int[] arr, int n) {
+        if(n == 0)
+            return arr[0];
+
+        return arr[n] > find_max(arr, n - 1) ? arr[n] : find_max(arr, n - 1);
+    }
+}
