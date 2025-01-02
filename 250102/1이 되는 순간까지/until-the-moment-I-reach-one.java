ca4fa5bc96e613bf5d4aad21a5da6d2d16bd0392
Y-hyundo <hyundo010488@naver.com>
2025-01-02 20:44:42 +0900

    Title: 1이 되는 순간까지, Time: 117ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 250102/1이 되는 순간까지/until-the-moment-I-reach-one.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        System.out.println(count_work(n));
+    }
+
+    public static int count_work(int n) {
+            if(n == 1)
+                return 0;
+
+            if(n % 2 == 0)
+                return count_work(n / 2) + 1;
+
+            else
+                return count_work(n / 3) + 1;
+    }
+}
