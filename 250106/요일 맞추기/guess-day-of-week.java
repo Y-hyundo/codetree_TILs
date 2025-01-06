1c2c085a260f44baa48bb66199d1ec179f60cd89
Y-hyundo <hyundo010488@naver.com>
2025-01-06 13:01:32 +0900

    Title: 요일 맞추기, Time: 3026ms, Memory: 10MB, Status: Time Limit Exceed - CodeTree

--- 250106/요일 맞추기/guess-day-of-week.java
+++ 250106/요일 맞추기/guess-day-of-week.java
@@ -11,8 +11,51 @@ public class Main {
         int m2 = sc.nextInt();
         int d2 = sc.nextInt();
 
-        int totalDays = ((m2 - m1) * 30) + (d2 - d1);
+        int totalDays = 0;
+
+        if(m2 == m1) {
+            totalDays = d2 - d1;
+        }
+        else {
+            while(true) {
+                if(m1 == m2 && d1 == d2)
+                    break;
+
+                if(m2 < m1) {
+                    d1--;
+                    totalDays--;
+
+                    if(d1 == 0) {
+                        m1--;
+                        d1 = getLastDay(m1);
+                        totalDays--;
+                    }
+                }
+
+                else {
+                    d1++;
+                    totalDays++;
+
+                    if(d1 == getLastDay(m1)) {
+                        m1++;
+                        d1 = 1;
+                        totalDays++;
+                    }
+                }
+            }
+        }
+        
         int today = (7 + totalDays) % 7;
         System.out.println(days[today]);
+    }
+
+    public static int getLastDay(int m) {
+        if(m == 2)
+            return 28;
+        
+        if(m == 4 || m == 6 || m == 9 || m == 11)
+            return 30;
+
+        return 31;
     }
 }
