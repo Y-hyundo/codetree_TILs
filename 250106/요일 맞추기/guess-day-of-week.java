3bbadd018ad7d8c4678f6d1fe5364644eda23b53
Y-hyundo <hyundo010488@naver.com>
2025-01-06 12:50:17 +0900

    Title: 요일 맞추기, Time: 125ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 250106/요일 맞추기/guess-day-of-week.java
+++ 250106/요일 맞추기/guess-day-of-week.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         // Please write your code here.
         Scanner sc = new Scanner(System.in);
-        String[] days = new String[]{"Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"};
+        String[] days = new String[]{"Mon", "The", "Wed", "Thu", "Fri", "Sat", "Sun"};
 
         int m1 = sc.nextInt();
         int d1 = sc.nextInt();
@@ -12,14 +12,7 @@ public class Main {
         int d2 = sc.nextInt();
 
         int totalDays = ((m2 - m1) * 30) + (d2 - d1);
-        if(totalDays < 0) {
-            totalDays *= -1;
-            totalDays %= 6;
-            System.out.println(days[1 - totalDays]);
-            return;
-        }
-        
-        totalDays %= 6;
-        System.out.println(days[1 + totalDays]);
+        int today = (7 + totalDays) % 7;
+        System.out.println(days[today]);
     }
 }
