6abd093c111f09c97c468bfeb1524a46bf29596a
Y-hyundo <hyundo010488@naver.com>
2025-01-06 16:16:13 +0900

    Title: 최대로 겹치는 구간, Time: 131ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 250106/최대로 겹치는 구간/maximum-overlapped-segments.java
+++ 250106/최대로 겹치는 구간/maximum-overlapped-segments.java
@@ -8,31 +8,19 @@ public class Main {
         int n = sc.nextInt();
         int[] a = new int[n];
         int[] b = new int[n];
-        int[] panel = new int[200];
 
         for(int i = 0; i < n; i++) {
-            int x1 = sc.nextInt();
-            int x2 = sc.nextInt();
-            if(x1 < 0)
-                x1 += 100;
-            if(x2 < 0)
-                x2 += 100;
-            a[i] = x1;
-            b[i] = x2;
+            a[i] = sc.nextInt();
+            b[i] = sc.nextInt();
         }
 
-        for(int i = 0; i < n; i++) {
-            for(int j = a[i]; j < b[i]; j++) {
-                panel[j]++;
-            }
+        int count = 1;
+
+        for(int i = 0; i < n - 1; i++) {
+            if(b[i] > a[i + 1])
+                count++;
         }
 
-        int max = 0;
-        for(int i = 0; i < panel.length; i++) {
-            if(panel[i] > max)
-                max = panel[i];
-        }
-        
-        System.out.println(max);
+        System.out.println(count);
     }
 }
