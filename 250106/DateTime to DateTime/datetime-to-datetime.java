e68e78a3c8637ddb487bd20e4f2df4b5fdc7f474
Y-hyundo <hyundo010488@naver.com>
2025-01-06 12:06:11 +0900

    Title: DateTime to DateTime, Time: 130ms, Memory: 10MB, Status: Passed - CodeTree

--- 250106/DateTime to DateTime/datetime-to-datetime.java
+++ 250106/DateTime to DateTime/datetime-to-datetime.java
@@ -10,6 +10,10 @@ public class Main {
         int min = sc.nextInt();
 
         int totalTime = ((day - 11) * 24 * 60) + ((hour - 11) * 60) + (min - 11);
+        if(totalTime < 0) {
+            System.out.println(-1);
+            return;
+        }
         System.out.println(totalTime);
     }
 }
