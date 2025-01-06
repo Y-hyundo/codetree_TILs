d10a991f3d0dfe2827092ccbddb25e5b0b533775
Y-hyundo <hyundo010488@naver.com>
2025-01-06 11:43:27 +0900

    Title: Date to Date, Time: 3028ms, Memory: 10MB, Status: Time Limit Exceed - CodeTree

---
+++ 250106/Date to Date/date-to-date.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+
+        int m1 = sc.nextInt();
+        int d1 = sc.nextInt();
+        int m2 = sc.nextInt();
+        int d2 = sc.nextInt();
+
+        int elapsedDays = 0;
+
+        int m1_lastDay = getLastDay(m1);
+        elapsedDays += m1_lastDay - d1 + 1;
+
+        while(true) {
+            m1++;
+            if(m1 == m2)
+                break;
+
+            elapsedDays += getLastDay(m1);
+        }
+        
+        elapsedDays += d2;
+        System.out.println(elapsedDays);
+    }
+
+    public static int getLastDay(int m) {
+        if(m == 2)
+            return 28;
+
+        if(m == 3 || m == 6 || m == 9 || m == 11)
+            return 30;
+        
+        return 31;
+    }
+}
