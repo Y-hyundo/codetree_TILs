14390d2bf086222b865f8a366bbe7010aadd7966
Y-hyundo <hyundo010488@naver.com>
2025-01-07 23:54:30 +0900

    Title: 사각형의 총 넓이 2, Time: 120ms, Memory: 10MB, Status: Passed - CodeTree

--- 250107/사각형의 총 넓이 2/total-width-of-a-rectangle2.java
+++ 250107/사각형의 총 넓이 2/total-width-of-a-rectangle2.java
@@ -1,18 +1,20 @@
 import java.util.*;
 
 public class Main {
+    public static final int OFFSET = 100;
+    public static final int MAX_R = 200;
     public static void main(String[] args) {
         // Please write your code here.
         Scanner sc = new Scanner(System.in);
 
         int n = sc.nextInt();
-        int[][] arr = new int[201][201];
+        int[][] arr = new int[MAX_R + 1][MAX_R + 1];
         
         for(int i = 0; i < n; i++) {
-            int x1 = sc.nextInt() + 100;
-            int y1 = sc.nextInt() + 100;
-            int x2 = sc.nextInt() + 100;
-            int y2 = sc.nextInt() + 100;
+            int x1 = sc.nextInt() + OFFSET;
+            int y1 = sc.nextInt() + OFFSET;
+            int x2 = sc.nextInt() + OFFSET;
+            int y2 = sc.nextInt() + OFFSET;
 
             for(int j = x1; j < x2; j++) {
                 for(int k = y1; k < y2; k++) {
@@ -22,9 +24,9 @@ public class Main {
         }
 
         int width = 0;
-        for(int i = 0; i < 201; i++) {
-            for(int j = 0; j < 201; j++) {
-                if(arr[i][j] == 1)
+        for(int i = 0; i <= MAX_R; i++) {
+            for(int j = 0; j <= MAX_R; j++) {
+                if(arr[i][j] > 0)
                     width++;
             }
         }
