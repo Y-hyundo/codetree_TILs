63b893787bb99504bc50cb638c8722b5e51fb27c
Y-hyundo <hyundo010488@naver.com>
2025-01-08 16:53:31 +0900

    Title: 잔해물을 덮기 위한 사각형의 최소 넓이, Time: 163ms, Memory: 26MB, Status: Wrong Answer - CodeTree

---
+++ 250108/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.java
@@ -0,0 +1,59 @@
+import java.util.*;
+
+public class Main {
+    public static final int OFFSET = 1000;
+    public static final int MAX_R = 2000;
+
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+
+        int[][] rect = new int[MAX_R + 1][MAX_R + 1];
+        int[] x1 = new int[2];
+        int[] y1 = new int[2];
+        int[] x2 = new int[2];
+        int[] y2 = new int[2];
+        
+        
+        for(int i = 0; i < 2; i++) {
+            x1[i] = sc.nextInt() + OFFSET;
+            y1[i] = sc.nextInt() + OFFSET;
+            x2[i] = sc.nextInt() + OFFSET;
+            y2[i] = sc.nextInt() + OFFSET;
+
+            for(int j = x1[i]; j < x2[i]; j++) {
+                for(int k = y1[i]; k < y2[i]; k++) {
+                    if(i == 1) {
+                        rect[j][k] = 0;
+                    }
+                    else {
+                        rect[j][k] = 1;
+                    }
+                }
+            }
+        }
+
+        int min_x = 0;
+        int min_y = 0;
+        int max_x = 0;
+        int max_y = 0;
+        
+        for(int i = x1[0]; i < x2[0]; i++) {
+            for(int j = y1[0]; j < y2[0]; j++) {
+                if(rect[i][j] == 1 && min_x == 0 && min_y == 0) {
+                    min_x = i;
+                    min_y = j;
+                }
+                else {
+                    max_x = i;
+                    max_y = j;
+                }
+            }
+        }
+
+        int width = max_x - min_x + 1;
+        int height = max_y - min_y + 1;
+
+        System.out.println(width * height);
+    }
+}
