486c33f62a67103199f5d293bd811e5a86125bce
Y-hyundo <hyundo010488@naver.com>
2025-01-09 15:58:58 +0900

    Title: T를 초과하는 연속 부분 수열, Time: 184ms, Memory: 12MB, Status: Passed - CodeTree

---
+++ 250109/T를 초과하는 연속 부분 수열/subsequence-above-t.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int t = sc.nextInt();
+        int[] arr = new int[n];
+
+        for(int i = 0; i < n; i++)
+            arr[i] = sc.nextInt();
+
+        int max = 0, cnt = 0;
+        for(int i = 0; i < n; i++) {
+            if(i >= 1 && arr[i] > t)
+                cnt++;
+            else
+                cnt = 0;
+    
+            max = Math.max(max, cnt);
+        }
+
+        System.out.println(max);
+    }
+}
