ec9df1e52c25fe0558063decf042af7423a61611
Y-hyundo <hyundo010488@naver.com>
2025-01-09 15:55:14 +0900

    Title: 연속되는 수 3, Time: 163ms, Memory: 11MB, Status: Passed - CodeTree

--- 250109/연속되는 수 3/continuous-number3.java
+++ 250109/연속되는 수 3/continuous-number3.java
@@ -12,7 +12,7 @@ public class Main {
         
         // 연속하여 동일한 부호의 숫자가 나오는 횟수를 구해보며,
         // 그 중 최댓값을 갱신
-        int ans = 0, cnt = 0;
+        int max = 0, cnt = 0;
         for(int i = 0; i < n; i++) {
             // Case 1
             if(i >= 1 && arr[i] * arr[i - 1] > 0)
@@ -21,9 +21,9 @@ public class Main {
             else
                 cnt = 1;
             
-            ans = Math.max(ans, cnt);
+            max = Math.max(max, cnt);
         }
         
-        System.out.print(ans);
+        System.out.print(max);
     }
 }
