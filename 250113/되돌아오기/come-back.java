2db90d86b8fc35b765468449d43faaea48a76318
Y-hyundo <hyundo010488@naver.com>
2025-01-13 22:42:56 +0900

    Title: 되돌아오기, Time: 116ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 250113/되돌아오기/come-back.java
+++ 250113/되돌아오기/come-back.java
@@ -1,22 +1,16 @@
 import java.util.*;
 
 public class Main {
-    public static final int MAX = 2000;
-
     public static void main(String[] args) {
         // Please write your code here.
         Scanner sc = new Scanner(System.in);
 
         int n = sc.nextInt();
-        
-        int[][] arr = new int[MAX + 1][MAX + 1];
         int[] dx = new int[]{0, 1, 0, -1};
         int[] dy = new int[]{1, 0, -1, 0};
-        int x = 1000, y = 1000, dir = 0;
+        int x = 0, y = 0, dir = 0;
         int second = 0;
 
-
-        arr[x][y] = 1;
         for(int i = 0; i < n; i++) {
             String d = sc.next();
             int move = sc.nextInt();
@@ -31,14 +25,13 @@ public class Main {
                 dir = 3;
 
             for(int j = 0; j < move; j++) {
-                x = x + dx[dir];
-                y = y + dy[dir];
+                x += dx[dir];
+                y += dy[dir];
                 second++;
                 if(x == 1000 && y == 1000) {
                     System.out.println(second);
                     return;
                 }
-                arr[x][y] = 1;
             }
         }
 
