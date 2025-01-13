21a6f757c39063fbb435eda87951795cccfbc356
Y-hyundo <hyundo010488@naver.com>
2025-01-13 22:41:15 +0900

    Title: 되돌아오기, Time: 140ms, Memory: 26MB, Status: Passed - CodeTree

--- 250113/되돌아오기/come-back.java
+++ 250113/되돌아오기/come-back.java
@@ -2,7 +2,6 @@ import java.util.*;
 
 public class Main {
     public static final int MAX = 2000;
-    public static final int OFFSET = 1000;
 
     public static void main(String[] args) {
         // Please write your code here.
@@ -35,7 +34,7 @@ public class Main {
                 x = x + dx[dir];
                 y = y + dy[dir];
                 second++;
-                if(arr[x][y] == 1) {
+                if(x == 1000 && y == 1000) {
                     System.out.println(second);
                     return;
                 }
