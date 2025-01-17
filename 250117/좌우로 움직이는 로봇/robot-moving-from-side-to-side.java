38e718c5247af12ab43c17b26343d991a1c0cae9
Y-hyundo <hyundo010488@naver.com>
2025-01-17 23:03:42 +0900

    Title: 좌우로 움직이는 로봇, Time: 1550ms, Memory: 90MB, Status: Passed - CodeTree

--- 250117/좌우로 움직이는 로봇/robot-moving-from-side-to-side.java
+++ 250117/좌우로 움직이는 로봇/robot-moving-from-side-to-side.java
@@ -2,7 +2,6 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        // Please write your code here.
         Scanner sc = new Scanner(System.in);
 
         int n = sc.nextInt();
@@ -14,58 +13,50 @@ public class Main {
         int indexA = 0;
         int indexB = 0;
 
-        for(int i = 0; i < n; i++) {
+        moveA.add(0);
+        moveB.add(0);
+
+        // A의 이동 기록
+        for (int i = 0; i < n; i++) {
             int t = sc.nextInt();
             String d = sc.next();
 
-            if(d.equals("R")) {
-                for(int j = 0; j < t; j++) {
-                    indexA++;
-                    moveA.add(indexA);
-                }
-            } else {
-                for(int j = 0; j < t; j++) {
-                    indexA--;
-                    moveA.add(indexA);
-                }
+            int direction = d.equals("R") ? 1 : -1;
+            for (int j = 0; j < t; j++) {
+                indexA += direction;
+                moveA.add(indexA);
             }
         }
 
-        for(int i = 0; i < m; i++) {
+        // B의 이동 기록
+        for (int i = 0; i < m; i++) {
             int t = sc.nextInt();
             String d = sc.next();
 
-            if(d.equals("R")) {
-                for(int j = 0; j < t; j++) {
-                    indexB++;
-                    moveB.add(indexB);
-                }
-            } else {
-                for(int j = 0; j < t; j++) {
-                    indexB--;
-                    moveB.add(indexB);
-                }
+            int direction = d.equals("R") ? 1 : -1;
+            for (int j = 0; j < t; j++) {
+                indexB += direction;
+                moveB.add(indexB);
             }
         }
 
         int cnt = 0;
-        for(int i = 0; i < Math.max(moveA.size(), moveB.size()); i++) {
-            if(i >= moveA.size()) {
-                int valueA = moveA.get(moveA.size() - 1);
-                if(valueA == moveB.get(i) && valueA != moveB.get(i - 1))
-                    cnt++;
-                continue;
-            }
-            
-            if(i >= moveB.size()) {
-                int valueB = moveB.get(moveB.size() - 1);
-                if(valueB == moveA.get(i) && valueB != moveA.get(i - 1))
-                    cnt++;
-                continue;
-            }
-        
-            if(i != 0 && moveB.get(i) == moveA.get(i) && moveB.get(i - 1) != moveA.get(i - 1))
+        int maxTime = Math.max(moveA.size(), moveB.size());
+
+        // 마지막 위치를 유지하도록 설정
+        int lastA = moveA.get(moveA.size() - 1);
+        int lastB = moveB.get(moveB.size() - 1);
+
+        for (int i = 1; i < maxTime; i++) {
+            int posA = i < moveA.size() ? moveA.get(i) : lastA;
+            int posB = i < moveB.size() ? moveB.get(i) : lastB;
+
+            int prevA = i - 1 < moveA.size() ? moveA.get(i - 1) : lastA;
+            int prevB = i - 1 < moveB.size() ? moveB.get(i - 1) : lastB;
+
+            if (posA == posB && prevA != prevB) {
                 cnt++;
+            }
         }
 
         System.out.println(cnt);
