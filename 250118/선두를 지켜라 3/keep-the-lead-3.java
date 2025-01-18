06c9f4196766ea3d33bf2ff8cdc6fb5e6d70be6d
Y-hyundo <hyundo010488@naver.com>
2025-01-18 21:28:52 +0900

    Title: 선두를 지켜라 3, Time: 265ms, Memory: 26MB, Status: Wrong Answer - CodeTree

---
+++ 250118/선두를 지켜라 3/keep-the-lead-3.java
@@ -0,0 +1,66 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int indexA = 0;
+        int indexB = 0;
+
+        ArrayList<Integer> moveA = new ArrayList<>();
+        ArrayList<Integer> moveB = new ArrayList<>();
+
+        moveA.add(0);
+        moveB.add(0);
+
+        for(int i = 0; i < n; i++) {
+            int v = sc.nextInt();
+            int t = sc.nextInt();
+
+            for(int j = 0; j < t; j++) {
+                indexA += v;
+                moveA.add(indexA);
+            }
+        }
+
+        for(int i = 0; i < m; i++) {
+            int v = sc.nextInt();
+            int t = sc.nextInt();
+
+            for(int j = 0; j < t; j++) {
+                indexB += v;
+                moveB.add(indexB);
+            }
+        }
+
+        int cnt = 0;
+
+        String winner;
+        if(moveA.get(0) > moveB.get(0))
+            winner = "A";
+        else if(moveA.get(0) == moveB.get(0))
+            winner = "AB";
+        else
+            winner = "B";
+
+        for(int i = 1; i < moveA.size(); i++) {
+            String current_winner;
+            if(moveA.get(i) > moveB.get(i))
+                current_winner = "A";
+            else if(moveA.get(i) == moveB.get(i))
+                current_winner = "AB";
+            else
+                current_winner = "B";
+
+            if(!current_winner.equals(winner)) {
+                cnt++;
+                winner = current_winner;
+            }
+        }
+
+        System.out.println(cnt);
+    }
+}
